48f16c6af68ba93141e53e73ee7a058a960387e7
Vinicius Nascimento <vinicius_1571@outlook.com>
2024-02-28 17:30:37 -0300

    Finalizando tela funcionarios e crriando classe Funcionario DAO

---
+++ br.com.projeto.MODEL/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle_de_Vendas.br.com.projeto.MODEL
+{
+    public class Funcionario : Cliente
+    {
+        public string senha { get; set; }
+        public string cargo { get; set; }
+        public string nivel_acesso { get; set; }
+
+    }
+}
